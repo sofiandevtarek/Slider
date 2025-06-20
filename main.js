c9c0af0d0a58599b37a7bcad966376f52ce15f86
AL Tarek Sofian dev <sofiandev815@gmail.com>
2025-06-20 19:35:33 +0300

    u

---
+++ main.js
@@ -0,0 +1,10 @@
+const slider = document.querySelector(".slidee");
+const dots = document.querySelector(".dots");
+let currentIndex = 0;
+
+function showSlide(index) {
+  slider.array.forEach((element) => {
+    slider.classList.remove("active");
+    dots[i].classList.remove("active");
+  });
+}
